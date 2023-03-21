7a7c6c489a58ba48e6b8114ee46010be9a48c05b
enileva <avelinemc@gmail.com>
2023-03-21 10:07:44 -0500

    Basic nav bar created

--- index.php
+++ index.php
@@ -9,6 +9,13 @@
     </head>
     <header>
         <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Ministries</a></li>
+                <li><a href="">Give</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
         </nav>
     </header>
     <div class="content">
